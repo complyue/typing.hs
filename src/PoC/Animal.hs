0fda72f793a7d7a8646712a03c63927ee11fdef4
ComplYue <compl.yue@icloud.com>
2021-09-02 21:43:33 +0800

    improved domain model

--- src/PoC/Animal.hs
+++ src/PoC/Animal.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
 module PoC.Animal where
 
+import Control.Applicative
+import Control.Monad
 import Type.Reflection
 import Prelude
 
@@ -11,7 +18,7 @@ data Sound = Miaow | Wafu
 data CatColour = Orange | White | Black
   deriving (Eq, Show)
 
-data Species = FelisCatus | CanisFamiliaris
+data Species = FelisCatus | CanisFamiliaris | Cryptodira
   deriving (Eq, Show)
 
 -- * Classification types (classes)
@@ -50,62 +57,89 @@ instance Animal Cat where
     SomeAnimal $
       AnimalType @Cat
         ($ typeRep)
-        (\na app -> app typeRep)
-        (\na _app -> na)
+        ($ typeRep)
+        {- HLINT ignore "Use const" -}
+        {- 'const' really won't work here, with strange error:
+            Could not deduce (Winged Cat) from the context
+        -}
+        -- (const mzero)
+        (\_ -> mzero)
 
 instance Mammal Cat where
   isFurry _ = True
   makesSound _ = Miaow
 
--- * comprehension types
-
-data AnimalType a = AnimalType
-  { animal'type'holder ::
-      forall r.
-      (forall a'. (a' ~ a, Animal a', Typeable a') => TypeRep a -> r) ->
-      r,
-    animal'type'as'of'mamal ::
-      forall r.
-      r ->
-      (forall a'. (a' ~ a, Mammal a', Typeable a') => TypeRep a -> r) ->
-      r,
-    animal'type'as'of'winged ::
-      forall r.
-      r ->
-      (forall a'. (a' ~ a, Winged a', Typeable a') => TypeRep a -> r) ->
-      r
+data Tortoise = Tortoise
+  { tortoise_name :: String,
+    tortoise_age :: Float
   }
 
-data SomeAnimal = forall a. SomeAnimal (AnimalType a) a
+instance Animal Tortoise where
+  getName = tortoise_name
+  getSpecies = const Cryptodira
+  getAge = tortoise_age
+
+  animalAsOf =
+    SomeAnimal $
+      AnimalType @Tortoise
+        ($ typeRep)
+        (\_ -> mzero)
+        (\_ -> mzero)
+
+-- * comprehension types
+
+data AnimalType a = (Animal a, Typeable a) =>
+  AnimalType
+  { with'animal'type ::
+      forall m r.
+      (MonadPlus m) =>
+      (TypeRep a -> m r) ->
+      m r,
+    with'mamal'type ::
+      forall m r.
+      (MonadPlus m) =>
+      (forall a'. (a' ~ a, Mammal a', Typeable a') => TypeRep a -> m r) ->
+      m r,
+    with'winged'type ::
+      forall m r.
+      (MonadPlus m) =>
+      (forall a'. (a' ~ a, Winged a', Typeable a') => TypeRep a -> m r) ->
+      m r
+  }
+
+data SomeAnimal
+  = forall a. (Animal a, Typeable a) => SomeAnimal (AnimalType a) a
 
 -- * demo usage
 
 -- | Polymorphic Animal examination
 vet :: SomeAnimal -> IO ()
 vet (SomeAnimal t a) = do
-  animal'type'holder t $ \(_ :: TypeRep a) -> do
-    -- here GHC can witness a's 'Animal' instance, dynamically
-    putStrLn $
-      "Let's see what " <> getName a <> " really is ..."
-    putStrLn $
-      "It is a " <> show (getSpecies a) <> "."
-  animal'type'as'of'mamal
-    t
-    (putStrLn "We know it's not a mammal.")
-    $ \(_ :: TypeRep a) -> do
+  -- a's 'Animal' instance is apparent, which is witnessed even statically
+  putStrLn $
+    "Let's see what " <> getName a <> " really is ..."
+  putStrLn $
+    "It is a " <> show (getSpecies a) <> "."
+
+  (<|> putStrLn "We know it's not a mammal.") $
+    with'mamal'type t $ \(_ :: TypeRep a) -> do
       -- here GHC can witness a's 'Mammal' instance, dynamically
       putStrLn $
         "It's a mammal that "
           <> if isFurry a then "furry." else " with no fur."
       putStrLn $
         "It says \"" <> show (makesSound a) <> "\"."
-  animal'type'as'of'winged
-    t
-    (putStrLn "We know it's not winged.")
-    $ \(_ :: TypeRep a) -> do
+
+  (<|> putStrLn "We know it's not winged.") $
+    with'winged'type t $ \(_ :: TypeRep a) -> do
       -- here GHC can witness a's 'Winged' instance, dynamically
       putStrLn $
         "It's winged "
           <> if flys a then "and can fly." else "but can't fly."
       putStrLn $
         "It " <> if feathered a then "does" else "doesn't" <> " have feather."
+
+main :: IO ()
+main = do
+  vet $ animalAsOf $ Cat "Doudou" 1.2 Orange False
+  vet $ animalAsOf $ Tortoise "Khan" 101.5
